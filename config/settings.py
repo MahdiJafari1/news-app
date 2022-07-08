d8f45ce70f70c581e4e1318dbfba85704c07bb7e
Mahdi Jafari <mahdi100th@gmail.com>
2022-07-08 18:26:24 +0430

    comments

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,16 +41,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
     # 3rd Party
     "crispy_forms",
     "crispy_bootstrap5",
-    
     # Locals
     "accounts",
     "pages",
     "articles",
-    
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
@@ -142,4 +143,4 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
